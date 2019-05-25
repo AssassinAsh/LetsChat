b24e30aca07291d4facebbfbe8772b4b8050c2e1
AssassinAsh <ashvinrokade@gmail.com>
2019-05-25 23:45:30 +0530

    Add newMessage and createMessage methods

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to Server');
+
+    socket.emit('createMessage', {
+        from:"Bron",
+        text:"Fook off!"
+    });
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from Server');
+});
+
+socket.on('newMessage', function (message){
+    console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New User Connected");
 
+    socket.emit('newMessage', {
+        from: "Tyrion",
+        text: "A Lannister always pays his debt.",
+        createdAt:69
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('Client Disconnected');
     });
