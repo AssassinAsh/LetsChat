0a6aaa26924914b4d083e690b51d884a6bd44eed
AssassinAsh <ashvinrokade@gmail.com>
2019-05-26 14:49:11 +0530

    setting basic messenging

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to Server');
-
-    socket.emit('createMessage', {
-        from:"Bron",
-        text:"Fook off!"
-    });
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log("New User Connected");
 
-    socket.emit('newMessage', {
-        from: "Tyrion",
-        text: "A Lannister always pays his debt.",
-        createdAt:69
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
