ed9c1bcd703ba500e8f111b7aabed7e984aab69f
AssassinAsh <ashvinrokade@gmail.com>
2019-05-28 23:16:20 +0530

    First message

--- server/server.js
+++ server/server.js
@@ -19,11 +19,11 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
 
         io.emit('newMessage', generateMessage(message.from, message.text));
-
+        callback('Server Ka Akg');
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
         //     text: message.text,
